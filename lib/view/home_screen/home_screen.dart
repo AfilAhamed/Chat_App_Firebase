c366b32b94d702afe525894e9b2f3b6705cfb5c2
AfilAhamed <afilahamed0007@gmail.com>
2023-12-18 15:40:40 +0530

    first commit

---
+++ lib/view/home_screen/home_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Chat App',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
+        ),
+        leading: IconButton(onPressed: () {}, icon: const Icon(Icons.home)),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search_rounded)),
+          IconButton(
+              onPressed: () {}, icon: const Icon(Icons.more_vert_outlined))
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blue,
+        onPressed: () {},
+        elevation: 0,
+        tooltip: 'Message',
+        splashColor: Colors.lightBlue,
+        child: const Icon(
+          Icons.message_rounded,
+          color: Colors.white,
+          size: 29,
+        ),
+      ),
+    );
+  }
+}
