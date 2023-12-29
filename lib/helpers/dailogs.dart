d6cb5f51cbb64729dc1fda945b72eac99402c543
AfilAhamed <afilahamed0007@gmail.com>
2023-12-29 17:27:19 +0530

    Profile Completed

--- lib/helpers/dailogs.dart
+++ lib/helpers/dailogs.dart
@@ -15,6 +15,8 @@ class Dailogas {
   }
 
   showBottomSheet(context) {
+    final mq = MediaQuery.of(context).size;
+
     showModalBottomSheet(
         shape: const RoundedRectangleBorder(
             borderRadius: BorderRadius.only(
@@ -22,10 +24,34 @@ class Dailogas {
         context: context,
         builder: (builder) {
           return ListView(
-            children: const [
-              Text(
+            shrinkWrap: true,
+            padding:
+                EdgeInsets.only(top: mq.height * .03, bottom: mq.height * .05),
+            children: [
+              const Text(
                 'Pick a Profile Picture',
-                style: TextStyle(fontSize: 20),
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: mq.height * .02),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          shape: const CircleBorder(),
+                          fixedSize: Size(mq.width * .3, mq.height * .15)),
+                      onPressed: () {},
+                      child: Image.asset('assets/google-logo.png')),
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          shape: const CircleBorder(),
+                          fixedSize: Size(mq.width * .3, mq.height * .15)),
+                      onPressed: () {},
+                      child: Image.asset('assets/google-logo.png')),
+                ],
               )
             ],
           );
